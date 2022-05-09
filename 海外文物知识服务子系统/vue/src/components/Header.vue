3a85c0da981afe95a4ae0b5cd1104c998d8ce711
koumenmian <95416678+koumenmian@users.noreply.github.com>
2022-05-09 20:50:28 +0800

    添加时间轴，修改一点界面，完善数据库

--- 海外文物知识服务子系统/vue/src/components/Header.vue
+++ 海外文物知识服务子系统/vue/src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <div style="height: 50px; line-height: 50px; border-bottom: 1px solid #ccc; display: flex">
     <div style="width: 200px; padding-left: 30px; font-weight: bolder; color: dodgerblue">海外文物知识服务系统</div>
+
     <div style="flex: 1"></div>
 
     <div style="width:100px">
